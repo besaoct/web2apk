fcf37ab3e5cd3474158f0c33509151338e1397e5
Shafin <blackipie@outlook.com>
2022-04-02 13:50:05 +0530

    adtu

---
+++ app/src/main/java/com/app/adtu/MainActivity.java
@@ -0,0 +1,373 @@
+package com.app.adtu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.InputType;
+import android.util.Log;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.content.ContentValues.TAG;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebView;
+    //  for choosing files
+    private static String file_type     = "*/*";
+    private String cam_file_data = null;
+    private ValueCallback<Uri> file_data;
+    private ValueCallback<Uri[]> file_path;
+    private final static int file_req_code = 1;
+    //finish
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView=(WebView) findViewById(R.id.webview);
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.getSettings().setDomStorageEnabled(true);
+        mywebView.getSettings().setAllowContentAccess(true);
+        mywebView.getSettings().setAllowFileAccess(true);
+        mywebView.getSettings().setJavaScriptEnabled(true);
+        mywebView.setWebChromeClient(new WebChromeClient());
+        mywebView.loadUrl("https://my.adtu.in/");
+        WebSettings webSettings=mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        final SwipeRefreshLayout pullToRefresh = findViewById(R.id.pullToRefresh);
+        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mywebView.loadUrl("https://my.adtu.in/");
+                pullToRefresh.setRefreshing(false);
+            }
+        });
+
+        mywebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("upi:")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+
+
+        mywebView.setWebChromeClient(new WebChromeClient() {
+            @SuppressLint("NewApi")
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("Please Confirm")
+                        .setMessage(message)
+                        .setPositiveButton("Yes", (DialogInterface dialog, int which) -> result.confirm())
+                        .setOnDismissListener((DialogInterface dialog) -> result.confirm())
+                        .create()
+                        .show();
+                return true;
+            }
+
+            @SuppressLint("NewApi")
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("Please Confirm")
+                        .setMessage(message)
+                        .setPositiveButton("Yes", (DialogInterface dialog, int which) -> result.confirm())
+                        .setNegativeButton("Cancel", (DialogInterface dialog, int which) -> result.cancel())
+                        .setOnDismissListener((DialogInterface dialog) -> result.cancel())
+                        .create()
+                        .show();
+                return true;
+            }
+
+            @SuppressLint("NewApi")
+            @Override
+            public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+                final EditText input = new EditText(view.getContext());
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                input.setText(defaultValue);
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("Please Confirm")
+                        .setMessage(message)
+                        .setView(input)
+                        .setPositiveButton("Yes", (DialogInterface dialog, int which) -> result.confirm(input.getText().toString()))
+                        .setNegativeButton("Cancel", (DialogInterface dialog, int which) -> result.cancel())
+                        .setOnDismissListener((DialogInterface dialog) -> result.cancel())
+                        .create()
+                        .show();
+                return true;
+            }
+
+
+            //new
+            /*-- handling input[type="file"] requests for android API 21+ --*/
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+
+                if(file_permission() && Build.VERSION.SDK_INT >= 21) {
+                    file_path = filePathCallback;
+                    Intent takePictureIntent = null;
+                    Intent takeVideoIntent = null;
+
+                    boolean includeVideo = false;
+                    boolean includePhoto = false;
+
+                    /*-- checking the accept parameter to determine which intent(s) to include --*/
+
+                    paramCheck:
+                    for (String acceptTypes : fileChooserParams.getAcceptTypes()) {
+                        String[] splitTypes = acceptTypes.split(", ?+");
+                        /*-- although it's an array, it still seems to be the whole value; split it out into chunks so that we can detect multiple values --*/
+                        for (String acceptType : splitTypes) {
+                            switch (acceptType) {
+                                case "*/*":
+                                    includePhoto = true;
+                                    includeVideo = true;
+                                    break paramCheck;
+                                case "image/*":
+                                    includePhoto = true;
+                                    break;
+                                case "video/*":
+                                    includeVideo = true;
+                                    break;
+                            }
+                        }
+                    }
+
+                    if (fileChooserParams.getAcceptTypes().length == 0) {
+
+                        /*-- no `accept` parameter was specified, allow both photo and video --*/
+
+                        includePhoto = true;
+                        includeVideo = true;
+                    }
+
+                    if (includePhoto) {
+                        takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if (takePictureIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
+                            File photoFile = null;
+                            try {
+                                photoFile = create_image();
+                                takePictureIntent.putExtra("PhotoPath", cam_file_data);
+                            } catch (IOException ex) {
+                                Log.e(TAG, "Image file creation failed", ex);
+                            }
+                            if (photoFile != null) {
+                                cam_file_data = "file:" + photoFile.getAbsolutePath();
+                                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                            } else {
+                                cam_file_data = null;
+                                takePictureIntent = null;
+                            }
+                        }
+                    }
+
+                    if (includeVideo) {
+                        takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                        if (takeVideoIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
+                            File videoFile = null;
+                            try {
+                                videoFile = create_video();
+                            } catch (IOException ex) {
+                                Log.e(TAG, "Video file creation failed", ex);
+                            }
+                            if (videoFile != null) {
+                                cam_file_data = "file:" + videoFile.getAbsolutePath();
+                                takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(videoFile));
+                            } else {
+                                cam_file_data = null;
+                                takeVideoIntent = null;
+                            }
+                        }
+                    }
+
+                    Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                    contentSelectionIntent.setType(file_type);
+
+
+                    Intent[] intentArray;
+                    if (takePictureIntent != null && takeVideoIntent != null) {
+                        intentArray = new Intent[]{takePictureIntent, takeVideoIntent};
+                    } else if (takePictureIntent != null) {
+                        intentArray = new Intent[]{takePictureIntent};
+                    } else if (takeVideoIntent != null) {
+                        intentArray = new Intent[]{takeVideoIntent};
+                    } else {
+                        intentArray = new Intent[0];
+                    }
+
+                    Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                    chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                    chooserIntent.putExtra(Intent.EXTRA_TITLE, "File chooser");
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                    startActivityForResult(chooserIntent, file_req_code);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+
+            //finish
+        });
+
+
+    }
+
+    //          new
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(Build.VERSION.SDK_INT >= 21){
+            Uri[] results = null;
+
+            /*-- if file request cancelled; exited camera. we need to send null value to make future attempts workable --*/
+            if (resultCode == Activity.RESULT_CANCELED) {
+                file_path.onReceiveValue(null);
+                return;
+            }
+
+            /*-- continue if response is positive --*/
+            if(resultCode== Activity.RESULT_OK){
+                if(null == file_path){
+                    return;
+                }
+                ClipData clipData;
+                String stringData;
+
+                try {
+                    clipData = intent.getClipData();
+                    stringData = intent.getDataString();
+                }catch (Exception e){
+                    clipData = null;
+                    stringData = null;
+                }
+                if (clipData == null && stringData == null && cam_file_data != null) {
+                    results = new Uri[]{Uri.parse(cam_file_data)};
+                }else{
+                    if (clipData != null) {
+                        final int numSelectedFiles = clipData.getItemCount();
+                        results = new Uri[numSelectedFiles];
+                        for (int i = 0; i < clipData.getItemCount(); i++) {
+                            results[i] = clipData.getItemAt(i).getUri();
+                        }
+                    } else {
+                        try {
+                            Bitmap cam_photo = (Bitmap) intent.getExtras().get("data");
+                            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                            cam_photo.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                            stringData = MediaStore.Images.Media.insertImage(this.getContentResolver(), cam_photo, null, null);
+                        }catch (Exception ignored){}
+
+                        results = new Uri[]{Uri.parse(stringData)};
+                    }
+                }
+            }
+
+            file_path.onReceiveValue(results);
+            file_path = null;
+        }else{
+            if(requestCode == file_req_code){
+                if(null == file_data) return;
+                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                file_data.onReceiveValue(result);
+                file_data = null;
+            }
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+    }
+    public boolean file_permission(){
+        if(Build.VERSION.SDK_INT >=23 && (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
+            return false;
+        }else{
+            return true;
+        }
+    }
+    private File create_image() throws IOException{
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img_"+timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName,".jpg,.png,.jpeg",storageDir);
+    }
+    private File create_video() throws IOException {
+        @SuppressLint("SimpleDateFormat")
+        String file_name    = new SimpleDateFormat("yyyy_mm_ss").format(new Date());
+        String new_name     = "file_"+file_name+"_";
+        File sd_directory   = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(new_name, ".3gp,.mp4", sd_directory);
+    }
+
+
+//     finish
+
+
+    public class mywebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mywebView.canGoBack()) {
+            mywebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+}
